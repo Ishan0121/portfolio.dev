169571eb48c863e22fb31f84a80e38362a219f03
Specter <ishanmaiti1234@gmail.com>
2026-06-25 09:58:37 +0530

    improve scrolling

--- src/components/SmoothScrolling.tsx
+++ src/components/SmoothScrolling.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from "lenis/react";
 
 export function SmoothScrolling({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, smoothWheel: true, syncTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, smoothWheel: true, wheelMultiplier: 1.2, touchMultiplier: 2 }}>
       {children}
     </ReactLenis>
   );
